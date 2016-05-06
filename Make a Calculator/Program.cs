e01bee8a55ffc5bcdc43b20677468b566eba0ec1
Your Name <your@example.com>
2016-05-06 14:06:05 -0400

    Make_A_Calculator_Initial

---
+++ Make a Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Make_a_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number:");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Operator");
+            string Operator = Console.ReadLine();
+            Console.WriteLine("Enter Another Number:");
+            int n2 = int.Parse(Console.ReadLine());
+            switch(Operator)
+            {
+                case "+":
+                    Console.WriteLine(n1 + n2);
+                    break;
+                case "-":
+                    Console.WriteLine(n1 - n2);
+                    break;
+                case "*":
+                    Console.WriteLine(n1 * n2);
+                    break;
+                case "/":
+                    Console.WriteLine(n1 / n2);
+                    break;
+                case "%":
+                    Console.WriteLine(n1 % n2);
+                    break;
+                case "^":
+                    Console.WriteLine(Math.Pow(n1, n2));
+                    break;
+                default:
+                    Console.WriteLine("Not an Operator, Please try again!");
+                    break;
+
+
+
+                
+
+            }
+
+
+
+        }
+    }
+}
